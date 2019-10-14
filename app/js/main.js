07bf45e7e0f960eceb7be0b31cefc53244f0bb93
St4y-23 <ghost19011@gmail.com>
2019-10-14 17:54:30 +0400

    added header

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
 $(function(){
 
-
+    // $('.slider-top').slick({
+    //     arrows: true,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     centerMode: false,
+    //     variableWidth: false,
+    //   });
 
   
 });
