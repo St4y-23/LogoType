512e5868ae0d362e46f934b978523ddfaa8c8a40
St4y-23 <ghost19011@gmail.com>
2019-10-21 17:54:31 +0400

    added services and about

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,12 @@
 $(function(){
 
-    // $('.slider-top').slick({
-    //     arrows: true,
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    //     centerMode: false,
-    //     variableWidth: false,
-    //   });
+    $('.slider-top-wrapper').slick({
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: false,
+        variableWidth: false,
+      });
 
   
 });
