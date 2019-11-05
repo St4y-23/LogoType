1b8c9ef95b634d306f49b5f6cbd130ff5c73d55e
St4y-23 <ghost19011@gmail.com>
2019-11-05 17:52:55 +0400

    added news slider

--- app/js/main.js
+++ app/js/main.js
@@ -8,5 +8,16 @@ $(function(){
         variableWidth: false,
       });
 
+    $('.news__inner').slick({
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        variableWidth: true,
+        prevArrow: '<button type="button" class="slick-prev icon-angle-left"></button>',
+        nextArrow: '<button type="button" class="slick-next icon-angle-right"></button>',
+      });
+
+     
+
   
 });
