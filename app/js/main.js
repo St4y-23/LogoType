ab8fad2d980d14127575f70bcaa858bbcea614d6
St4y-23 <ghost19011@gmail.com>
2019-12-24 12:28:59 +0400

    finish adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,48 @@
-$(function(){
+$(function () {
 
-    $('.slider-top-wrapper').slick({
-        arrows: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        centerMode: false,
-        variableWidth: false,
-				fade: true,
-				prevArrow: '<button type="button" class="slick-prev icon-angle-left"></button>',
-        nextArrow: '<button type="button" class="slick-next icon-angle-right"></button>',
-      });
+	$('.slider-top-wrapper').slick({
+		arrows: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		centerMode: false,
+		variableWidth: false,
+		fade: true,
+		prevArrow: '<button type="button" class="slick-prev icon-angle-left"></button>',
+		nextArrow: '<button type="button" class="slick-next icon-angle-right"></button>',
+		responsive: [
+			{
+				breakpoint: 1500,
+				settings: {
+					arrows: false,
+					dots: true,
+				}
+			},
+		]
+	});
 
-    $('.news__inner').slick({
-        arrows: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        variableWidth: true,
-        prevArrow: '<button type="button" class="slick-prev icon-angle-left"></button>',
-        nextArrow: '<button type="button" class="slick-next icon-angle-right"></button>',
-      });
+	$('.news__inner').slick({
+		arrows: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		variableWidth: true,
+		prevArrow: '<button type="button" class="slick-prev icon-angle-left"></button>',
+		nextArrow: '<button type="button" class="slick-next icon-angle-right"></button>',
+		responsive: [
+			{
+				breakpoint: 1100,
+				settings: {
+					arrows: false,
+					dots: true
+				}
+			},
+		]
+	});
 
-     
+	$('.ham').on('click', function (){
+		$('.header__menu').slideToggle()
+	});
 
-  
+
+
+
 });
